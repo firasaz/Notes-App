2701ced0f50b53f015f6600d6cc932e2db61b4e6
firasaz <fabuzahra@seapay.com.sa>
2024-06-20 02:53:28 +0300

    added firebase integration and fetching of notes from db and displaying to user if per note id

--- app/notes/[note].jsx
+++ app/notes/[note].jsx
@@ -1,9 +1,13 @@
+import { FIRESTORE_DB } from "@/FirebaseConfig"
+import firestore from '@react-native-firebase/firestore'
+import { addDoc, collection, getDocs } from 'firebase/firestore'
 import { useLocalSearchParams } from "expo-router"
-import { useState } from "react"
-import { View, Text, TextInput, FlatList } from "react-native"
+import { useEffect, useState } from "react"
+import { View, Text, TextInput, TouchableOpacity, ScrollView, ActivityIndicator } from "react-native"
 
-function note({ id }) {
+function note() {
   const [input, setInput] = useState('')
+  const [loading, setLoading] = useState(true)
   const noteId = useLocalSearchParams()
   const notes = [
     {
@@ -15,10 +19,41 @@ function note({ id }) {
       text: 'note 2'
     },
   ]
+
+
+  const handleSubmit = () => {
+    addDoc(collection(FIRESTORE_DB, 'notes_list'), { text: input, title: 'Note 3' })
+    .then(() => console.log('Note successfully added'))
+    .catch(() => console.log('an error occured...'))
+  }
+  const fetchNotes = async () => {
+    const data = await getDocs(collection(FIRESTORE_DB, 'notes_list'))
+    if(data) {
+      data.forEach(doc => {
+        if(doc?.data()?.id == noteId.note){
+          setInput(doc?.data()?.text)
+          setLoading(false)
+          return
+        }
+        setLoading(false)
+      })
+    } else {
+      console.log('something went wrong...')
+    }
+  }
+  useEffect(() => {
+    fetchNotes()
+  }, [])
   return (
     // <TextInput multiline={true} className='border mx-3 h-auto' />
-    <View className='h-full divide-y divide-dotted'>
-      <TextInput multiline={true} textAlignVertical="top" placeholder="Type your notes..." className='h-full mx-1 text-xl' defaultValue={input} onChange={(text)=>setInput({text})} />
+    <View className={!loading ? 'bg-yellow-100 h-full flex justify-between pb-7' : 'bg-yellow-100 h-full flex justify-center items-center'}>
+      {!loading ? 
+        <TextInput multiline={true} textAlignVertical="top" placeholder="Type your notes..." className='h-full text-xl' defaultValue={input} onChangeText={(text) => {setInput(text)}} />
+        : <ActivityIndicator size={'large'} color='#808080' />
+      }
+      {!loading ?? <TouchableOpacity onPress={handleSubmit} style={{ borderWidth: 1 }}>
+        <Text>Save</Text>
+      </TouchableOpacity>}
     </View>
   )
 }
